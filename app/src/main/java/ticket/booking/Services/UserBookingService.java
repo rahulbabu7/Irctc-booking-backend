e24f578404916df366c93391c76a6db08955ae72
rahulbabu7 <rahulbabu436@gmail.com>
2025-05-09 16:57:50 +0530

    added

--- app/src/main/java/ticket/booking/Services/UserBookingService.java
+++ app/src/main/java/ticket/booking/Services/UserBookingService.java
@@ -2,4 +2,10 @@ package ticket.booking.Services;
 
 public class UserBookingService {
     
+
+    private User user;
+
+    public UserBookingService(User user){
+        this.user = user;
+    }
 }
--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -1,11 +1,11 @@
 package ticket.booking.entities;
-import java.util.List;
+import java.util.ArrayList;
 
 public class User {
     private String name;
     private String password;
     private String hashPassword;
-    private List<Ticket> ticketsBooked;
+    private ArrayList<Ticket> ticketsBooked;
     private String userId;
 
 }
